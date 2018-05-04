ebd335e09a3b1cde3c83efc0ef9abec9a50f7fd4
Glauter Santos <glauterlima@gmail.com>
2018-05-04 19:19:51 -0300

    Aula 3.4 concluido - Configurando viewResolver com Thymeleaf

--- src/main/java/br/jus/trf1/aletheia/controller/ContagensController.java
+++ src/main/java/br/jus/trf1/aletheia/controller/ContagensController.java
@@ -1,8 +1,13 @@
 package br.jus.trf1.aletheia.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContagensController {
-
+	
+	@RequestMapping("/contagens/novo")
+	public String novo() {
+		return "contagem/CadastroContagem";
+	}
 }
